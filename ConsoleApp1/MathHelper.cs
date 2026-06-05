8216fee24b562f5962e7472f3e1d0b30448d5513
Ahmed Turky <ahmed.turky@dbsmena.com>
2026-06-05 20:01:56 +0300

    Change `Sum` method to perform subtraction instead
    The `Sum` method in the `MathHelper` class was modified to return the difference of two integers (`a - b`) instead of their sum (`a + b`). This change alters the method's behavior and may impact any code relying on it for addition.

--- ConsoleApp1/MathHelper.cs
+++ ConsoleApp1/MathHelper.cs
@@ -2,6 +2,6 @@ namespace ConsoleApp1
 {
     public static class MathHelper
     {
-        public static int Sum(int a, int b) => a + b;
+        public static int Sum(int a, int b) => a - b;
     }
 }
